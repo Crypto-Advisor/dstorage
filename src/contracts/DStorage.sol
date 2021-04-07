531d758d2a63a54e8eced757b80bb8087ec7c518
Crypto-Advisor <adriennavarre@hotmail.com>
2021-04-06 23:34:40 -0700

    day 1 progress 28:54

--- src/contracts/DStorage.sol
+++ src/contracts/DStorage.sol
@@ -1,37 +1,50 @@
 pragma solidity ^0.5.0;
 
 contract DStorage {
-  // Name
-  // Number of files
-  // Mapping fileId=>Struct 
+  string public name = "DStorage";
+  unit public fileCount = 0;
+  mapping (uint => File) public files;
 
   // Struct
-
+  struct FUle {
+    uint fileId;
+    string fileHash;
+    uint fileSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address payable uploader;
+  }
 
   // Event
 
   constructor() public {
   }
 
-  // Upload File function
+  function uploadFile(string memory _fileHash, uint _fileSize, string memory _fileType, string memory _fileName, string memory _fileDescription) public {
+    fileCount++;
+    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, now, msg.sender);
+      // Make sure the file hash exists
 
-    // Make sure the file hash exists
+      // Make sure file type exists
 
-    // Make sure file type exists
+      // Make sure file description exists
 
-    // Make sure file description exists
+      // Make sure file fileName exists
 
-    // Make sure file fileName exists
+      // Make sure uploader address exists
 
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
+      // Make sure file size is more than 0
 
 
-    // Increment file id
+      // Increment file id
 
-    // Add File to the contract
+      // Add File to the contract
 
-    // Trigger an event
+      // Trigger an event
+
+  }
+
 
 }
